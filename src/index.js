73f6cd1bfd887acdb501700a81819f70be268042
cklob23 <klobe.caleb23@gmail.com>
2025-07-29 15:48:57 -0400

    update cors url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,11 @@ const router = express.Router();
 const app = express();
 const PORT = 4000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://one01church-growth-track.onrender.com",
+  })
+);
 app.use(bodyParser.json());
 app.use(express.json());
 //app.use("/send", sendRoute);
