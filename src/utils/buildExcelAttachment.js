78a63cbdec2f5a5a279eadebdef40a90eb04121c
cklob23 <klobe.caleb23@gmail.com>
2025-09-22 09:02:20 -0400

    Update excel mapping

--- src/utils/buildExcelAttachment.js
+++ src/utils/buildExcelAttachment.js
@@ -29,7 +29,7 @@ export async function buildExcelAttachment(gifts) {
         const ministryIndexes = giftToMinistryMap[giftCode] || [];
 
         if (ministryIndexes.length === 0) {
-            worksheet.addRow({ gift, group: "❌ No Ministries Found", task: "-" });
+            worksheet.addRow({ gift, group: "No minitry groups are associated with this gift", task: "-" });
         } else {
             ministryIndexes.forEach((index) => {
                 worksheet.addRow({
